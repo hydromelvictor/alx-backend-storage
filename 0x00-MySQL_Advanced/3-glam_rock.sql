36f5851096d50bca684bf4ab82766ca75b28982c
Victor Doledji <victorvaddely@gmail.com>
2023-05-04 12:02:40 +0000

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
 -- Write a SQL script that lists all bands
 -- with Glam rock as their main style
-SELECT `band_name`, SUM(`style`)
+SELECT `band_name`, (IFNULL(`split`, NOW()) - formed) AS lifespan
 FROM `metal_bands`
-WHERE `style` = 'Glam rock';
+WHERE `style` LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
