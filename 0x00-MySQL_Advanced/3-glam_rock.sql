c547608f779804496fb9cf703d42a6b373e0cccf
Victor Doledji <victorvaddely@gmail.com>
2023-05-04 12:21:35 +0000

    Update 3-glam_rock.sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- Write a SQL script that lists all bands
 -- with Glam rock as their main style
-SELECT `band_name`, (IFNULL(`split`, 2023) - formed) AS lifespan
+SELECT `band_name`, (IFNULL(`split`, 2023) - `formed`) AS lifespan
 FROM `metal_bands`
 WHERE `style` LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
