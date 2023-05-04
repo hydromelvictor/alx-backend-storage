efc08632c246ec5ce6866d04471c0282c030bd4b
harkinder-dark <victorvaddely@gmail.com>
2023-05-04 17:03:26 +0000

    task 10

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,10 +1,6 @@
 -- Write a SQL script that creates a function
 -- SafeDiv that divides (and returns) the first
-DELIMITER $$
 DROP FUNCTION IF EXISTS SafeDiv;
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT DETERMINISTIC
-BEGIN
-    RETURN (IF (b = 0, 0, b / a ));
-END$$
-DELIMITER ;
+RETURN (IF (b = 0, 0, b / a ));
