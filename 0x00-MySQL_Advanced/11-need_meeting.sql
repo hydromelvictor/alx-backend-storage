5dc89c3e05959fdda3a56aaad82f57707770d308
harkinder-dark <victorvaddely@gmail.com>
2023-05-04 17:18:14 +0000

    task 11

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,7 +1,10 @@
 -- 
 -- 
-CREATE VIEW need_meeting (
-    `students`.`score` < 80
-    AND DATEDIFF(NOW(), `students`.`last_meeting`) IN (Now(), )
-    )
-AS SELECT * FROM `students`;
+CREATE VIEW need_meeting AS
+    SELECT `name`
+    FROM `students`
+    WHERE `students`.`score` < 80
+    AND
+    `students`.`last_meeting` IS NULL
+    OR
+    DATE_ADD(NOW(), INTERVAL -1 MONTH);
