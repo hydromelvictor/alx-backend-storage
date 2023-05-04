5f2698a4553234f41b8395806c56b94df8874f25
Victor Doledji <victorvaddely@gmail.com>
2023-05-04 11:23:16 +0000

    Update 3-glam_rock.sql
    task 2

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- Write a SQL script that lists all bands
 -- with Glam rock as their main style
-SELECT `band_name`, `style`
+SELECT `band_name`, SMU(`style`)
 FROM `metal_bands`
 WHERE `style` = 'Glam rock';
