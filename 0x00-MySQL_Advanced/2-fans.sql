120d14e1aeaa82515c8fa00e6d3dac8700c790b6
Victor Doledji <victorvaddely@gmail.com>
2023-05-04 11:19:51 +0000

    Update 2-fans.sql
    task 2

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,6 @@
 -- Write a SQL script that ranks country origins
 -- ordered by the number of (non-unique)
-SELECT `origin`, COUNT(`fans`)
+SELECT `origin`, SUM(`fans`) as nb_fans
 FROM `metal_bands`
-GROUP BY `origin`;
+GROUP BY `origin`
+ORDER BY nb_fans DESC;
