a4170d7cc6c3fb44a3e04bc229a585479f50f002
harkinder-dark <victorvaddely@gmail.com>
2023-05-04 17:39:14 +0000

    task 13

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,13 @@
+-- Write a SQL script that creates a stored
+-- procedure ComputeAverageScoreForUser
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser()
+BEGIN
+    UPDATE `users`
+    SET average_score = (SELECT SUM(`corrections`.`score` * `projects`.`weight`)/SUM(`weight`)
+    FROM `corrections`
+    INNER JOIN `projects`
+    ON `projects`.`id` = `corrections`.`project_id`
+    WHERE `corrections`.`user_id` = `users`.`user_id`);
+END $$
+DELIMITER ;
