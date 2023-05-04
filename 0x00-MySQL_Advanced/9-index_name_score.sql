51c172523bbe73a30f5644846ca4d87427fd019b
harkinder-dark <victorvaddely@gmail.com>
2023-05-04 16:42:07 +0000

    task 7

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,4 @@
 -- Write a SQL script that creates an index
 -- idx_name_first_score on the table names
 CERATE INDEX idx_name_first_score
-ON `names` (name(1), score);
+ON `names` (name(1), `score`);
