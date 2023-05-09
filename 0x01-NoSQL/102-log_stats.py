428c1f0d37c8a9367c9ac20c399ff486b5c5333c
harkinder-dark <victorvaddely@gmail.com>
2023-05-09 16:24:21 +0000

    add new directory not available

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
-    collection = db.ngnix.find()
+    collection = db.ngnix
 
     print("{} logs\n\
           Methods:\n\
